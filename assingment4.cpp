fd354820ad5031f33a4de860ab632a737d49cf29
Shubham Patil <101404197+patil8381@users.noreply.github.com>
2024-01-13 10:51:28 +0530

    assingment

--- assingment4.cpp
+++ assingment4.cpp
@@ -13,26 +13,52 @@ using namespace std;
 class TMString
 {
 char *p;
+int length;
+
 public:
 void getString()
 {
-cout<<this->p<<endl;
+cout<<this->p;
 } 
 TMString()
 {
+cout<<"default constructor called"<<endl;
 this->p=NULL;
 }
 
 void operator=(char *g)
 {
-this->p=(char *)malloc(strlen(g)+1);
-this->p=g;
+this->length=strlen(g)+1;
+if(p!=NULL)
+{
+cout<<"memory released "<<endl;
+free(p);
+p=(char *)malloc(length);
+int i;
+for(i=0;i<length-1;i++)	p[i]=g[i];
+p[i]='\0';
+
+cout<<"p=Indore"<<endl;
+}
+else
+{
+p=(char *)malloc(length);
+cout<<"s=Ujjain"<<endl;
+
+int i;
+for(i=0;i<length-1;i++)
+{
+p[i]=g[i];
+}
+p[i]='\0';
+
+}
 }
 
 ~TMString()
 {
 cout<<"destructor called"<<endl;
-delete p;
+free(p);
 }
 };
 
@@ -41,17 +67,13 @@ ostream & operator<<(ostream &o,TMString &s)
 s.getString();
 return o;
 }
- 
-namespace thinkingmachines
-{
-TMString s;
-}
 
-using namespace thinkingmachines;
 int main()
 {
+TMString s;
 s="Ujjain";
-
+cout<<s<<endl;
+s="Indore Dewas";
 cout<<s<<endl;
 
 return 0;
