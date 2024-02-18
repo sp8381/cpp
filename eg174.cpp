2295a23ed8bf0b94cd987b3debbb3c19a71b0c50
Shubham Patil <101404197+patil8381@users.noreply.github.com>
2024-02-18 19:51:56 +0530

    great example of tmlist
    if ccc(const bbb &) and 
    ccc(const aaa &) 
    aaa class is the parent class of ccc and bbb 
    then 
    ccc(const bbb &) will be called if it is not present then ccc(const aaa &) method is called.
    look in the example

--- eg174.cpp
+++ eg174.cpp
@@ -1,32 +1,58 @@
 #include<iostream>
 using namespace std;
+class ccc;
 class aaa
 {
 public:
-virtual aaa & operator=(const aaa &)=0;
+aaa()
+{
+cout<<"Default constructor of aaa"<<endl;
+}
+aaa(const aaa &)
+{
+cout<<"Copy constructor of aaa"<<endl;
+}
+//virtual aaa & operator=(const aaa &)=0;
 };
 class bbb:public aaa
 {
-bbb & operator=(const aaa &)
+public:
+bbb()
 {
+cout<<"Default constructor of bbb"<<endl;
 }
-aaa & operator=(const aaa &)
+bbb(const bbb &)
 {
+cout<<"Copy constructor of bbb"<<endl;
+}
+bbb(const ccc &)
+{
+cout<<"Parameterised constructor of bbb with parameter ccc &"<<endl;
 }
 };
 class ccc:public aaa
 {
-ccc & operator=(const ccc &)
+public:
+ccc()
 {
+cout<<"Default constructor of ccc"<<endl;
 }
-aaa & operator=(const aaa &)
+ccc(const ccc &)
 {
+cout<<"Copy constructor of ccc"<<endl;
+}
+ccc(const aaa &)
+{
+cout<<"Parameterised constructor of ccc with parameter aaa"<<endl;
+}
+ccc(const bbb &)
+{
+cout<<"Parameterised constructor of ccc with parameter bbb"<<endl;
 }
 };
 int main()
 {
-bbb b;
-ccc c;
-b=c;
+bbb b;	//pehle base ka constructor fir derived ka constructor 
+ccc c(b);
 return 0;
 }
