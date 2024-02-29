300545a522bfcd7f896c0c104607ea51a3c22675
Shubham Patil <101404197+patil8381@users.noreply.github.com>
2024-03-01 01:29:40 +0530

    Implementing isValid() function in TMDate.cpp

--- TMDate.cpp
+++ TMDate.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<string.h>
 
 class TMDate
 {
@@ -11,9 +12,11 @@ int dayNumber;
 int toDayNumber();
 int isLeapYear(int y);
 void fromDayNumber();
+void isValidDate(const char *dateString,int *isValid,int *d,int *m,int *y);
 
 public:
 TMDate();
+TMDate(const char *dateString);
 int getDayOfMonth();
 int getMonth();
 int getYear();
@@ -38,6 +41,91 @@ this->month=now->tm_mon+1;
 this->year=now->tm_year+1900;
 toDayNumber();
 }
+TMDate::TMDate(const char *dateString)
+{
+int isValid;
+int d,m,y;
+isValidDate(dateString,&isValid,&d,&m,&y);
+if(isValid)
+{
+this->dayOfMonth=d;
+this->month=m;
+this->year=y;
+this->toDayNumber();
+}
+else
+{
+this->dayNumber=0;
+this->dayOfMonth=0;
+this->month=0;
+this->year=0;
+}
+}
+void TMDate::isValidDate(const char *dateString,int *isValid,int *d,int *m,int *y)
+{
+int dd,mm,yy;
+char sep;
+int sepIndex1,sepIndex2;
+int mth[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+*isValid=0;
+if(dateString==NULL) return;
+int x=strlen(dateString);
+if(x<8 || x>10) return;
+if(!(dateString[1]=='/' || dateString[2]=='/' || dateString[1]=='-' || dateString[2]=='-')) return;
+if(dateString[1]=='/' || dateString[1]=='-') sepIndex1=1;
+else sepIndex1=2;
+sep=dateString[sepIndex1];
+if(dateString[0]<48 || dateString[0]>57) return;
+if(sepIndex1==2 && (dateString[1]<48 || dateString[1]>57 || dateString[1]<48 || dateString[1]>57)) return ;
+if(sepIndex1==1)
+{
+dd=dateString[0]-48;
+if(dd==0) return;
+}
+else
+{
+dd=((dateString[0]-48)*10)+(dateString[1]-48);
+if(dd==0) return;
+}
+int i=sepIndex1+1;
+if(!(dateString[i+1]==sep || dateString[i+2]==sep)) return;
+if(dateString[i+1]==sep) sepIndex2=i+1;
+else sepIndex2=i+2;
+if(dateString[i]<48 || dateString[i]>57) return ;
+if(sepIndex2==i+2 && (dateString[i+1]<48 || dateString[i+1]>57)) return;
+if(sepIndex2==i+1) 
+{
+mm=dateString[i]-48;
+}
+else 
+{
+mm=((dateString[i]-48)*10)+(dateString[i+1]-48);
+}
+if(mm<1 || mm>12) return ;
+i=sepIndex2+1;
+yy=0;
+
+int r=0;
+while(r<=3)
+{
+if(dateString[i+r]<48 || dateString[i+r]>57) return;
+yy=(yy*10)+(dateString[i+r]-48);
+r++;
+}
+if(yy<1901) return;
+if(yy%400==0) mth[1]=29;
+else if(yy%100==0) mth[1]=28;
+else if(yy%4==0) mth[1]=29;
+else mth[1]=28;
+if(dd<1 || dd>mth[mm-1]) return;
+if(mm<1 || mm>12) return;
+*d=dd;
+*m=mm;
+*y=yy;
+*isValid=1;
+}
+
 int TMDate::toDayNumber()
 {
 int x=0;
@@ -149,7 +237,11 @@ cout<<date<<endl;
 cout<<date.getDayNumber()<<endl;
 date-=45002;
 cout<<date<<endl;
-
+TMDate d2;
+d2="10/12/2020";
+cout<<d2<<endl;
+d2+=20;
+cout<<d2<<endl;
 
 return 0;
 }
