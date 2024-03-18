e780de3d249a10f6f0a9b9abcc3cf81ca82612d4
Shubham Patil <101404197+patil8381@users.noreply.github.com>
2024-03-19 01:21:40 +0530

    tmio version 3 with fwrite and fread

--- tmiov3.cpp
+++ tmiov3.cpp
@@ -5,6 +5,7 @@
 #include<stdio.h>
 #include<ios>
 #include<limits>
+#include<stdlib.h>
 using namespace std;
 
 class OutputFileStream
@@ -16,6 +17,7 @@ int lastOperationFailed;
 public:
 const static int append=1;
 const static int overwrite=2;
+const static int binary=4;
 OutputFileStream()
 {
 this->f=NULL;
@@ -39,7 +41,8 @@ this->mode=-1;
 }
 this->lastOperationFailed=1;
 if(fileName==NULL) return;
-if(mode!=append && mode!=overwrite) return;
+if(mode!=append && mode!=overwrite && mode!=(append | binary) && mode!=(overwrite | binary)) return;
+this->mode=mode;
 if(mode==append)
 {
 f=fopen(fileName,"a");
@@ -47,6 +50,14 @@ f=fopen(fileName,"a");
 else if(mode==overwrite)
 {
 f=fopen(fileName,"w");
+}
+else if(mode==(append | binary))
+{
+f=fopen(fileName,"ab");
+}
+else if(mode==(overwrite | binary))
+{
+f=fopen(fileName,"wb");
 }
 if(f==NULL) return;
 lastOperationFailed=0;
@@ -99,36 +110,60 @@ this->lastOperationFailed=0;
 return *this;
 }
 
+//version 3 method 
+void write(char *stream,int size)
+{
+this->lastOperationFailed=1;
+if(this->f==NULL) return;
+fwrite(stream,size,1,this->f);
+this->lastOperationFailed=0;
+return;
+}
 };
 
 class InputFileStream
 {
 private:
 FILE *f;
+int mode;
 int lastOperationFailed;
 public:
+const static int binary=4;
+const static int reading=1;
 InputFileStream()
 {
+this->mode=-1;
 this->f=NULL;
 this->lastOperationFailed=0;
 }
-InputFileStream(const char *fileName)
+InputFileStream(const char *fileName,int mode)
 {
+this->mode=-1;
 this->f=NULL;
 this->lastOperationFailed=0;
-this->open(fileName);
+this->open(fileName,reading);
 }
-void open(const char *fileName)
+void open(const char *fileName,int mode)
 {
 if(this->f!=NULL)
 {
+this->mode=-1;
 fclose(this->f);
 this->f=NULL;
 }
 this->lastOperationFailed=1;
 if(fileName==NULL) return;
+this->mode=mode;
+if(mode!=reading && mode!=binary) return;
+this->mode=mode;
+if(this->mode==reading)
+{
 f=fopen(fileName,"r");
-if(f==NULL) return;
+}
+else
+{
+f=fopen(fileName,"rb");
+}
 lastOperationFailed=0;
 }
 int fail()
@@ -207,54 +242,93 @@ this->lastOperationFailed=0;
 num=atoi(a);
 return *this;
 }
-
+//version 3 methods
+void read(char *stream,int size)
+{
+this->lastOperationFailed=1;
+if(this->f==NULL) return;
+if(feof(this->f)) return;
+fread(stream,size,1,f);
+if(feof(this->f)) return;
+this->lastOperationFailed=0;
+}
 };
 
+class Student
+{
+private:
+int rollNumber;
+char name[21];
+char gender;
+public:
+void setRollNumber(int rollNumber)
+{
+this->rollNumber=rollNumber;
+}
+int getRollNumber()
+{
+return this->rollNumber;
+}
+void setName(const char *name)
+{
+strcpy(this->name,name);
+}
+void getName(char *name)
+{
+strcpy(name,this->name);
+}
+void setGender(char gender)
+{
+this->gender=gender;
+}
+char getGender()
+{
+return this->gender;
+}
+};
 void addStudent()
 {
 int rollNumber;
 string name;
 char gender;
-cout<<"Enter roll nnumber : ";
+cout<<"Enter a roll Numeber : ";
 cin>>rollNumber;
-cin.ignore(numeric_limits<streamsize>::max(),'\n');
+cin.ignore(1000,'\n');
 cout<<"Enter a name : ";
 getline(cin,name);
-cout<<"Enter Gender : ";
+cout<<"Enter gender : ";
 cin>>gender;
-
-OutputFileStream f("students.dat",OutputFileStream::append);
-
-f<<rollNumber;
-f<<" ";
-f<<name;
-f<<"\n";
-f<<gender;
+Student s;
+s.setRollNumber(rollNumber);
+s.setName(name.c_str());
+s.setGender(gender);
+OutputFileStream f("students.ppp",ios::app | ios::binary);
+f.write((char *)&s,sizeof(Student));
 f.close();
+cout<<"Student Added"<<endl;
 }
-
-void displayListOfStudent()
+void displayListOfStudents()
 {
-InputFileStream k("students.dat");
-if(k.fail())
+cout<<"Student List Module"<<endl;
+InputFileStream k("students.ppp",InputFileStream::binary);
+if(k.fail()) 
 {
 cout<<"No students added."<<endl;
-return;
+return ;
 }
-int rollNumber;
-string name;
-char gender;
+Student s;
 while(1)
 {
-k>>rollNumber;
+k.read((char *)&s,sizeof(Student));
 if(k.fail()) break;
-k>>name;
-k>>gender;
-cout<<"RollNumber : "<<rollNumber<<", Name : "<<name<<", Gender : "<<gender<<endl;
+cout<<"Roll Number : "<<s.getRollNumber()<<endl;
+char name[21];
+s.getName(name);
+cout<<"Name : "<<name<<endl;
+cout<<"Gender : "<<s.getGender()<<endl;
 }
 k.close();
 }
-
 int main()
 {
 int ch;
@@ -268,7 +342,7 @@ cout<<"Enter you choice : ";
 scanf("%d",&ch);
 while(m=getchar()!='\n');
 if(ch==1) addStudent();
-if(ch==2) displayListOfStudent();
+if(ch==2) displayListOfStudents();
 //if(ch!=1 || ch!=2) break;
 if(ch==3) break;
 }
